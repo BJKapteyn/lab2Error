248600840740fca0c0b602506b9cec9f9e8725a9
Brad <bjkapteyn@gmail.com>
2019-05-16 15:09:04 -0400

    its a commit

---
+++ lab2.js
@@ -0,0 +1,14 @@
+let length = document.getElementById("length");
+let width = document.getElementById("width");
+let height = document.getElementById("height");
+
+let lengthErr = document.getElementById("lengthErr");
+let widthErr = document.getElementById("widthErr");
+let heightErr = document.getElementById("heightErr");
+
+function calculateArea() {
+    let lVal = length.value;
+    let wVal = width.value;
+    let hVal = height.value;
+}
+
